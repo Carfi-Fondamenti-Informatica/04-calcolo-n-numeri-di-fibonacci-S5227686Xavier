216d6bdafa92e3376cb266883d4db891313d5454
S5227686Xavier <91663950+S5227686Xavier@users.noreply.github.com>
2022-11-23 14:54:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main() {
+   int numero;
+    cout<<"inserire numero dei termini della serie di fibonacci che si vuole calcolare:"<<endl;
+    cin>>numero;
+
+    int i=3;
+    if (numero>=2) {
+        int primo_numero=1;
+        int secondo_numero=1;
+        cout <<primo_numero<<endl<<secondo_numero<<endl;
+        while (i++ <= numero) {
+            cout << primo_numero + secondo_numero <<endl;
+            if (primo_numero < secondo_numero)
+                primo_numero += secondo_numero;
+            else
+                secondo_numero += primo_numero;
+        }
+    }else {
+        cout << "errore";
+    }
    return 0;
 }
